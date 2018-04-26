e071844f7d4011aad0f5ef31af3d32f120c63e67
Joe Jobes <jmrjobes@gmail.com>
2018-04-26 14:57:27 -0600

    Initial commit and bubble sort test

---
+++ test/bubblesort-test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert
+const bubbleSort = require('../bubblesort')
+
+describe('bubble sort functionality', () => {
+  context('bubble sort function', () => {
+    it('it sorts and array of numbers', () =.{
+      assert.deepEqual(bubbleSort([2, 3, 1, 5, 4]), [1, 2, 3, 4, 5])
+    })
+  })
+})
