5c48aea39c28f8b55cdf3b9a9344d04fe8105765
Joe Jobes <jmrjobes@gmail.com>
2018-04-29 06:50:16 -0600

    Add bubblesort

--- test/bubblesort-test.js
+++ test/bubblesort-test.js
@@ -1,9 +1,9 @@
 const assert = require('chai').assert
-const bubbleSort = require('../bubblesort')
+const bubbleSort = require('../lib/bubblesort')
 
 describe('bubble sort functionality', () => {
   context('bubble sort function', () => {
-    it('it sorts and array of numbers', () =.{
+    it('it sorts and array of numbers', () => {
       assert.deepEqual(bubbleSort([2, 3, 1, 5, 4]), [1, 2, 3, 4, 5])
     })
   })
